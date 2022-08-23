29c1ece5326f08239a5a28efe647e1d603f88d24
Joseph Roberts <bertrbrts@gmail.com>
2022-08-22 22:24:06 -0400

    submitting databinding assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+  userName = '';
+  buttonDisabled = true;
+
+  ngOnInit(): void {
+  }
+
+  onUpdateUsername(event: any){
+    this.buttonDisabled = (<HTMLInputElement>event.target).value === '';
+  }
+
+  onButtonClick(){
+    this.userName = '';
+    this.buttonDisabled = true;
+  }
 }
