013df6124cc5024f9903806aa72450da1544c663
Joseph Roberts <bertrbrts@gmail.com>
2022-08-25 22:27:49 -0400

    End of Section 3

--- src/app/recipies/recipe-list/recipe-list.component.ts
+++ src/app/recipies/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test recipe', 'https://upload.wikimedia.org/wikipedia/commons/1/18/Yummly_logo.png'),
+    new Recipe('A Test Recipe2', 'This is a test recipe2', 'https://upload.wikimedia.org/wikipedia/commons/1/18/Yummly_logo.png'),
+  ];
 
   constructor() { }
 
